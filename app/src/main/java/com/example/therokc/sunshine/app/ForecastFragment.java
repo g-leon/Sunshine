be3d8cd41e03ce40501701b7c9d224593455a4b0
Guianu Leon <guianu.leon@gmail.com>
2014-08-17 15:50:04 +0300

    Added a Refresh button in the action bar for debugging purposes.

--- app/src/main/java/com/example/therokc/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/therokc/sunshine/app/ForecastFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -31,6 +34,26 @@ public class ForecastFragment extends Fragment {
 	}
 
 	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setHasOptionsMenu(true);
+	}
+
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+		inflater.inflate(R.menu.forecastfragment, menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		int id = item.getItemId();
+		if (id == R.id.action_refresh) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
@@ -48,7 +71,6 @@ public class ForecastFragment extends Fragment {
 		mForecastAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, weekForecast);
 		ListView listView = (ListView) rootView.findViewById(listview_forecast);
 		listView.setAdapter(mForecastAdapter);
-
 		return rootView;
 	}
 
