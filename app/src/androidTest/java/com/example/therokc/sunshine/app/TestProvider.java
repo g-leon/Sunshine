aa8d1179c45a7fcdf97aad8e493e1b9b7340da4d
Guianu Leon <guianu.leon@gmail.com>
2014-08-25 16:09:30 +0300

    Implemented update() and delete() methods from WeatherProvider + tests.

--- app/src/androidTest/java/com/example/therokc/sunshine/app/TestProvider.java
+++ app/src/androidTest/java/com/example/therokc/sunshine/app/TestProvider.java
@@ -8,6 +8,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.Build;
 import android.test.AndroidTestCase;
+import android.util.Log;
 
 import com.example.therokc.sunshine.app.data.WeatherContract.LocationEntry;
 import com.example.therokc.sunshine.app.data.WeatherContract.WeatherEntry;
@@ -18,8 +19,43 @@ public class TestProvider extends AndroidTestCase {
 
 	public static final String LOG_TAG = TestProvider.class.getSimpleName();
 
-	public void testDeleteDb() throws Throwable {
-		mContext.deleteDatabase(WeatherDbHelper.DATABASE_NAME);
+	public void deleteAllRecords() {
+		mContext.getContentResolver().delete(
+				WeatherEntry.CONTENT_URI,
+				null,
+				null
+		);
+		mContext.getContentResolver().delete(
+				LocationEntry.CONTENT_URI,
+				null,
+				null
+		);
+
+		Cursor cursor = mContext.getContentResolver().query(
+				WeatherEntry.CONTENT_URI,
+				null,
+				null,
+				null,
+				null
+		);
+		assertEquals(0, cursor.getCount());
+		cursor.close();
+
+		cursor = mContext.getContentResolver().query(
+				LocationEntry.CONTENT_URI,
+				null,
+				null,
+				null,
+				null
+		);
+		assertEquals(0, cursor.getCount());
+		cursor.close();
+	}
+
+	// Since we want each test to start with a clean slate, run deleteAllRecords
+	// in setUp (called by the test runner before each test).
+	public void setUp() {
+		deleteAllRecords();
 	}
 
 	public void testInsertReadProvider() {
@@ -128,7 +164,8 @@ public class TestProvider extends AndroidTestCase {
 
 		String testDate = "20140612";
 		// content://com.example.therokc.sunshine.app/weather/94074/20140612
-		type = mContext.getContentResolver().getType(WeatherEntry.buildWeatherLocationWithDate(testLocation, testDate));
+		type = mContext.getContentResolver().getType(WeatherEntry.buildWeatherLocationWithDate(testLocation,
+		                                                                                       testDate));
 		// vnd.android.cursor.item/com.example.android.sunshine.app/weather
 		assertEquals(WeatherEntry.CONTENT_ITEM_TYPE, type);
 
@@ -143,6 +180,39 @@ public class TestProvider extends AndroidTestCase {
 		assertEquals(LocationEntry.CONTENT_ITEM_TYPE, type);
 	}
 
+	public void testUpdateLocation() {
+		// Create a new map of values, where column names are the keys
+		ContentValues values = TestDb.createNorthPoleLocationValues();
+		Uri locationUri = mContext.getContentResolver().insert(LocationEntry.CONTENT_URI, values);
+		long locationRowId = ContentUris.parseId(locationUri);
+
+		// Verify we got a row back.
+		assertTrue(locationRowId != -1);
+		Log.d(LOG_TAG, "New row id: " + locationRowId);
+		ContentValues updatedValues = new ContentValues(values);
+		updatedValues.put(LocationEntry._ID, locationRowId);
+		updatedValues.put(LocationEntry.COLUMN_CITY_NAME, "Santa's Village");
+		int count = mContext.getContentResolver().update(
+				LocationEntry.CONTENT_URI, updatedValues, LocationEntry._ID + "= ?",
+				new String[]{Long.toString(locationRowId)});
+		assertEquals(count, 1);
+
+		// A cursor is your primary interface to the query results.
+		Cursor cursor = mContext.getContentResolver().query(
+				LocationEntry.buildLocationUri(locationRowId),
+				null,
+				null, // Columns for the "where" clause
+				null, // Values for the "where" clause
+				null // sort order
+		);
+		TestDb.validateCursor(cursor, updatedValues);
+	}
+
+	// Make sure we can still delete after adding/updating stuff
+	public void testDeleteRecordsAtEnd() {
+		deleteAllRecords();
+	}
+
 	// The target api annotation is needed for the call to keySet -- we wouldn't want
 	// to use this in our app, but in a test it's fine to assume a higher target.
 	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
@@ -152,3 +222,4 @@ public class TestProvider extends AndroidTestCase {
 		}
 	}
 }
+
