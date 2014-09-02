96db13efdd503efa878e1b5ba9f9fcd3ea8e6075
Guianu Leon <guianu.leon@gmail.com>
2014-09-02 09:54:44 +0300

    Finished implementing the SyncAdapter.

--- app/src/main/java/com/example/therokc/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/therokc/sunshine/app/ForecastFragment.java
@@ -1,9 +1,5 @@
 package com.example.therokc.sunshine.app;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -23,7 +19,7 @@ import android.widget.ListView;
 import com.example.therokc.sunshine.app.data.WeatherContract;
 import com.example.therokc.sunshine.app.data.WeatherContract.LocationEntry;
 import com.example.therokc.sunshine.app.data.WeatherContract.WeatherEntry;
-import com.example.therokc.sunshine.app.service.SunshineService;
+import com.example.therokc.sunshine.app.sync.SunshineSyncAdapter;
 
 import java.util.Date;
 
@@ -156,21 +152,7 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
 	}
 
 	private void updateWeather() {
-		Intent alarmIntent = new Intent(getActivity(), SunshineService.AlarmReceiver.class);
-		alarmIntent.putExtra(SunshineService.LOCATION_QUERY_EXTRA, Utility.getPreferredLocation(getActivity()));
-
-		// Wrap in a pending intent which only fires once.
-		PendingIntent pi = PendingIntent.getBroadcast(getActivity(), 0, alarmIntent,
-		                                              PendingIntent.FLAG_ONE_SHOT);//getBroadcast(context, 0, i, 0);
-
-		AlarmManager am = (AlarmManager) getActivity().getSystemService(Context.ALARM_SERVICE);
-
-		// Set the AlarmManager to wake up the system.
-		am.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 5000, pi);
-
-		Intent intent = new Intent(getActivity(), SunshineService.class);
-		intent.putExtra(SunshineService.LOCATION_QUERY_EXTRA, Utility.getPreferredLocation(getActivity()));
-		getActivity().startService(intent);
+		SunshineSyncAdapter.syncImmediately(getActivity());
 	}
 
 	@Override
